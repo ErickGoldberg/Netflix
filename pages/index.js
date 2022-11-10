0a9ff4ab6725c70341cb1adecae89ef31f831ab6
Erick Goldberg <erick_goldberg@hotmail.com>
2022-11-10 11:04:14 -0300

    alterando componentes

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
+import Background from 'next/image'
+
+/*
+FUNCIONAR CSS NA HOME.JS
+background n ficar bzarro assim(responsivo)
+SE DESSE BOTAR ESSE MEIO COMO COMPONENTES
+*/
 
 export default function Home() {
   return (
+    <div>
+      <Background src="/background.webp" fill></Background>
         <div className={styles.container}>
         <h1 id={styles.titulo}>Entrar</h1>
             <input type="email" placeholder='Email' id={styles.input1} className={styles.box}></input>
@@ -16,6 +25,7 @@ export default function Home() {
             <label for="lembrar" id={styles.lembrar}>Lembre-se de mim</label>
             <p id={styles.robo}>Esta página é protegida pelo Google reCAPTCHA para garantir que você não é um robô <a id={styles.link_robo}>Saiba mais.</a></p>
         </div>
+    </div>
   )
 }
 
