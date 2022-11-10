74ff5dd7b7ca6fbae554b7f22dcbe5a77518da89
Erick Goldberg <erick_goldberg@hotmail.com>
2022-11-10 00:33:23 -0300

    First commit

---
+++ pages/home.js
@@ -0,0 +1,17 @@
+//Background color #141414
+
+export default function filmes(){
+    return(
+        <div>
+            <h1>Quem está assistindo?</h1>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+            <input type="submit" value="Gerenciar perfis"></input>
+        </div>
+    )
+}
